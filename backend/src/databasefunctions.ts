cae65e23db8ee5bdc109c8688013f349d171231b
Endre Ulberg <endremau@gmail.com>
2019-10-17 23:13:14 +0200

    made endpoint for generating accesstokens and for company logins

--- backend/src/databasefunctions.ts
+++ backend/src/databasefunctions.ts
@@ -1,33 +1,71 @@
-import * as pg from 'pg'
-require('dotenv').config()
+import * as pg from "pg"
+require("dotenv").config()
 
-const pgconfig = {
-    user: process.env.DBUSER,
-    database: process.env.DATABASENAME,
-    password: process.env.DBPASSWORD,
-    host: process.env.DBHOST,
-    port: process.env.DBPORT,
+
+
+const pgconfig: pg.PoolConfig = {
+  user: process.env.DBUSER,
+  database: process.env.DATABASENAME,
+  password: process.env.DBPASSWORD,
+  host: process.env.DBHOST,
+  port: parseInt(process.env.DBPORT)
 }
 
 const pool = new pg.Pool(pgconfig)
 
-pool.on('error', function(err: Error, client: pg.Client) {
-    console.error(err)
+
+pool.on("error", (err: Error,client: pg.PoolClient) => {
+  console.error(err,client)
 })
 
-export const createTable = async sqlStatement => {
-    if (!sqlStatement.toLowerCase().includes('create table')) {
-        throw new Error('wrong type of SQL statement')
-    }
-    const client = await pool.connect()
+
+export const createTable = async (sqlStatement: string) => {
+  if (!sqlStatement.toLowerCase().includes("create table")) {
+    throw new Error("wrong type of SQL statement")
+  }
+  const client = await pool.connect()
+  try {
+    const res = await client.query(sqlStatement)
+    return res
+  } finally {
     try {
-        const res = await client.query(sqlStatement)
-        return res
-    } finally {
-        try {
-            await client.end()
-        } catch (err) {
-            console.error(err)
-        }
+      client.release()
+    } catch (err) {
+      console.error(err)
     }
+  }
+}
+
+export const insertSingleRow = async (sqlStatement: string,data: string[]) => {
+  if (!sqlStatement.toLowerCase().includes("insert into")) {
+    throw new Error("wrong type of SQL statement")
+  }
+  const client = await pool.connect()
+  try {
+    const res = await client.query(sqlStatement,data)
+    return res
+  } finally {
+    try {
+      client.release()
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
+
+export const getRows = async (sqlStatement: string,data: string[] = []) => {
+  if (!sqlStatement.toLowerCase().includes("select")) {
+    throw new Error("wrong type of SQL statement")
+  }
+  const client = await pool.connect()
+  try {
+    const res = await client.query(sqlStatement,data)
+    return res
+  } finally {
+    try {
+      client.release()
+    } catch (err) {
+      console.error(err)
+    }
+  }
 }
