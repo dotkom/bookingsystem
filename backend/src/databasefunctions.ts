b92d50f130e04f03f08fd20983ceb64eac8c63ca
Endre Ulberg <endremau@gmail.com>
2019-10-17 23:05:54 +0200

    added Eslint + Prettier with support for typescript

--- backend/src/databasefunctions.ts
+++ backend/src/databasefunctions.ts
@@ -1,39 +1,33 @@
-import * as pg from 'pg';
-require('dotenv').config();
-
-
+import * as pg from 'pg'
+require('dotenv').config()
 
 const pgconfig = {
-  user: process.env.DBUSER,
-  database: process.env.DATABASENAME,
-  password: process.env.DBPASSWORD,
-  host: process.env.DBHOST,
-  port: process.env.DBPORT
+    user: process.env.DBUSER,
+    database: process.env.DATABASENAME,
+    password: process.env.DBPASSWORD,
+    host: process.env.DBHOST,
+    port: process.env.DBPORT,
 }
 
-const pool = new pg.Pool(pgconfig);
+const pool = new pg.Pool(pgconfig)
 
-
-pool.on('error', function (err:Error, client:pg.Client) {
+pool.on('error', function(err: Error, client: pg.Client) {
     console.error(err)
-});
+})
 
-  
-export const createTable = async (sqlStatement) => {
+export const createTable = async sqlStatement => {
     if (!sqlStatement.toLowerCase().includes('create table')) {
         throw new Error('wrong type of SQL statement')
     }
-    const client = await pool.connect();
+    const client = await pool.connect()
     try {
-      const res = await client.query(sqlStatement);
-      return res;
+        const res = await client.query(sqlStatement)
+        return res
     } finally {
-      try {
-        await client.end();
-      } catch (err) {
-        console.error(err)
-      }
+        try {
+            await client.end()
+        } catch (err) {
+            console.error(err)
+        }
     }
-  };
-
-
+}
