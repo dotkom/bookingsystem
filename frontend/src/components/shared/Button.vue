23508e601937494f6a85e18e4be2491176105974
adrianht <adrianhthompson@gmail.com>
2019-10-26 20:02:19 +0200

    add button component

---
+++ frontend/src/components/shared/Button.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="btnDiv">
+    <button @click="btnClick">
+      {{ btnText }}
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Button" as string,
+  props: {
+    btnText: String
+  },
+  methods: {
+    btnClick() {
+      this.$emit("btnClicked");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.btnDiv {
+  display: flex;
+  justify-content: space-around;
+  -moz-box-align: center;
+  align-items: center;
+  margin: 2rem 0px;
+}
+button {
+  background: rgb(0, 96, 163) none repeat scroll 0% 0%;
+  color: rgb(255, 255, 255);
+  border-radius: 4px;
+  padding: 0.8rem;
+  font-size: 1rem;
+  font-weight: 600;
+  letter-spacing: 1px;
+  display: flex;
+  margin: auto;
+}
+</style>
--- frontend/src/views/CompanyLogin.vue
+++ frontend/src/views/CompanyLogin.vue
@@ -16,6 +16,7 @@
         labelText="Kode"
         @emitInputText="updateAdmissionCode"
       />
+      <Button btnText="KOM I GANG" @btnClicked="btnClicked" />
     </div>
   </div>
 </template>
@@ -23,21 +24,26 @@
 <script>
 import TextInput from "@/components/shared/TextInput.vue";
 import Header from "@/components/shared/Header.vue";
+import Button from "@/components/shared/Button.vue";
 
 export default {
   components: {
     TextInput,
-    Header
+    Header,
+    Button
   },
   methods: {
     updateAdmissionCode(event) {
       this.$store.dispatch("SET_ADMISSION_CODE", event.target.value);
+    },
+    btnClicked() {
+      //button was clicked, do something
     }
   }
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .companyLogin {
   display: flex;
   flex-direction: column;
