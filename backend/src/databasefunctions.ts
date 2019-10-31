b877b959f18e50480247561e67f9ea6843e7cc13
endremau <endremau@gmail.com>
2019-10-31 10:23:16 +0100

    Squashed commits

---
+++ backend/src/databasefunctions.ts
@@ -0,0 +1,80 @@
+import { PoolConfig, ClientConfig } from 'pg';
+
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pgconfig: PoolConfig = {
+  user: process.env.DBUSER,
+  database: process.env.DATABASENAME,
+  password: process.env.DBPASSWORD,
+  host: process.env.DBHOST,
+  port: parseInt(process.env.DBPORT),
+};
+
+const pool = new Pool(pgconfig);
+
+pool.on('error', (err: Error, client: ClientConfig) => {
+  console.error(err, client);
+});
+export const createTable = async (sqlStatement: string) => {
+  if (
+    sqlStatement &&
+    !sqlStatement.toLowerCase().includes('create table')
+  ) {
+    throw new Error('wrong type of SQL statement');
+  }
+  let client;
+  try {
+    client = await pool.connect();
+    const res = await client.query(sqlStatement);
+    return res;
+  } finally {
+    try {
+      client.release();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+};
+
+export const insertSingleRow = async (
+  sqlStatement: string,
+  data: string[],
+) => {
+  if (!sqlStatement.toLowerCase().includes('insert into')) {
+    throw new Error('wrong type of SQL statement');
+  }
+  let client;
+  try {
+    client = await pool.connect();
+    const res = await client.query(sqlStatement, data);
+    return res;
+  } finally {
+    try {
+      client.release();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+};
+
+export const getRows = async (
+  sqlStatement: string,
+  data: string[] = [],
+) => {
+  if (!sqlStatement.toLowerCase().includes('select')) {
+    throw new Error('wrong type of SQL statement');
+  }
+  let client;
+  try {
+    client = await pool.connect();
+    const res = await client.query(sqlStatement, data);
+    return res;
+  } finally {
+    try {
+      client.release();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+};
