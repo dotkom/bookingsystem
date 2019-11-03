71f440c8bdd64c165d4e2234b372d549ec88c44e
endremau <endremau@gmail.com>
2019-11-03 22:31:27 +0100

    added a central error handler

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,19 +3,24 @@ import express = require('express');
 import { QueryResultRow } from 'pg';
 import cors from 'cors';
 import formidableMiddleware = require('express-formidable');
+import { handleError, ErrorHandler } from './helpers/error';
 
 // Create a new express application instance
 const app: express.Application = express();
 app.use(formidableMiddleware());
 app.use('*', cors());
 
-app.post('/accesstoken', (req, res) => {
-  const values = req.fields; // This is a string[]
-  insertSingleRow(
-    'insert into keys (accesstoken) VALUES ($1) on conflict do nothing',
-    [values[0] as string],
-  );
-  res.status(200).send({ status: 'success' });
+app.post('/accesstoken', (req, res, next) => {
+  try {
+    const values = req.fields; // This is a string[]
+    insertSingleRow(
+      'insert into keys (accesstoken) VALUES ($1) on conflict do nothing',
+      [values[0] as string],
+    );
+    res.status(200).send({ status: 'success' });
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.post('/company/login', async (req, res) => {
@@ -29,6 +34,13 @@ app.post('/company/login', async (req, res) => {
   res.status(200).send(data);
 });
 
+app.get('/error', (req, res) => {
+  throw new ErrorHandler(500, 'Internal server error');
+});
+
+app.use((err, req, res, next) => {
+  handleError(err, res);
+});
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
