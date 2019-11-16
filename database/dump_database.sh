58e6fe09ce79c1042362b78fcddaf1bafd6faac7
endremau <endremau@gmail.com>
2019-11-16 12:35:44 +0100

    updated the dumpscript to not be reliant on having postgresql on host machine

--- database/dump_database.sh
+++ database/dump_database.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 
 
-
-echo "Password is: password"
-pg_dump -a -h localhost -U postgres -p 5018 booking_dev > datadump.sql
-#The implmentation below should be better because it does not assume that you have postgresql on the host service. 
-#it is currently not working, would have to use pg_dumpall to function as intended, this will not allow the DockerImage to start.
-
-
-#docker exec -t bookingsystem_db pg_dump -c -U postgres > datadump.sqlecho "Dump completed"
-echo "Dump completed"
+docker exec -t bookingsystem_db pg_dump -h localhost -U postgres -p 5432 booking_dev > datadump.sql
