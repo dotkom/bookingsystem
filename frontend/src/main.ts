556f105e9ef9989ac6b89cb8216804d2220a24da
endremau <endremau@gmail.com>
2020-03-18 21:29:44 +0100

    Added Sentry to frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,9 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import * as Sentry from "@sentry/browser";
+import * as Integrations from "@sentry/integrations";
 
 Vue.config.productionTip = false;
 
+const DNS = process.env.VUE_APP_DNS || "";
+Sentry.init({
+  dsn: DNS,
+  integrations: [new Integrations.Vue({ Vue, attachProps: true })]
+});
 new Vue({
   router,
   store,
